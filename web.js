16ae7de398c58b26e58a4eccfee5b34409906aa9
Volta-36 <158546333+Volta-36@users.noreply.github.com>
2024-02-02 11:21:48 +0545

    Add files via upload

---
+++ web.js
@@ -0,0 +1,17 @@
+function readmore(){
+    var readmore = document.getElementById('secondary-id');
+    readmore.scrollIntoView({behavior: "smooth"});
+}
+
+function shikshadeepfb(){
+    window.location.href = 'https://www.facebook.com/shikshadeephsschool';
+}
+
+function shikshadeepyt(){
+    window.location.href = 'https://www.youtube.com/@shikshadeepcollege2421';
+}
+
+function programs(){
+    var programs = document.getElementById('programs-id');
+    programs.scrollIntoView({behavior: "smooth"});
+}
